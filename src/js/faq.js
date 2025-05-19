96cfff2f0b8724d0ef4f575aff5dcb0263353b61
Igor <igor.vasuyk45@gmail.com>
2025-05-19 17:38:56 +0100

    delete js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,1 @@
-import Accordion from 'accordion-js';
 
-import 'accordion-js/dist/accordion.min.css';
-
-new Accordion('.accordion-container', {
-  duration: 1000,
-  showMultiple: true,
-  elementClass: 'accordion-item',
-  triggerClass: 'accordion-header',
-  panelClass: 'accordion-content',
-  activeClass: 'is-active',
-});
